abe3d2ab6c04e22fb74a23710a2e4d6da363aa01
nikhil-chowdhury-vst-au4 <nikhilchowdhury666@gmail.com>
2020-12-26 19:31:24 +0530

    first commit

---
+++ build/precache-manifest.6eafa3b8f84b7f94a333c8f0e6f9582c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2fdb1533ea049d4df5acaa6e954a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9186895ef8b6943f404f",
+    "url": "/static/css/main.3e9a850e.chunk.css"
+  },
+  {
+    "revision": "1c70485e7cfd1e1a9bac",
+    "url": "/static/js/2.f76bf850.chunk.js"
+  },
+  {
+    "revision": "9186895ef8b6943f404f",
+    "url": "/static/js/main.aebd9ee2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
